cd5a9e17f0f839ee92c9c117cfa4bff03f9a1a2d
Shahar <shaharl@sternumiot.com>
2022-04-28 19:39:51 +0300

    Second commit

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 tar -cf liv-shell-scripts.tar *.sh
-jfrog rt u liv-shell-scripts.tar liv-operation/liv-shell-scripts/liv-shell-scripts-$(git rev-parse HEAD | cut -c1-8).tar
-jfrog rt u liv-shell-scripts.tar liv-operation/liv-shell-scripts/liv-shell-scripts-latest.tar
+jf rt u liv-shell-scripts.tar liv-operation/liv-shell-scripts/liv-shell-scripts-$(git rev-parse HEAD | cut -c1-8).tar
+jf rt u liv-shell-scripts.tar liv-operation/liv-shell-scripts/liv-shell-scripts-latest.tar
+rm liv-shell-scripts.tar
